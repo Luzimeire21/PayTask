58aaffedf9d776179a1c3be68c1201ab70d708fa
Dev2025 <victorfazekas3@gmail.com>
2024-11-29 16:57:00 -0300

    projectCard and service adjustment

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Card, CardHeader } from "@/components/ui/card";
-import { CircleDollarSignIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CalendarDaysIcon, CircleDollarSignIcon } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { ServiceTable } from "@/drizzle/schema";
+import { formatPriceToReais } from "@/lib/formatters";
 
 const ProjectCard = ({
   project,
@@ -13,9 +21,14 @@ const ProjectCard = ({
   isServiceCard: boolean;
 }) => {
   return (
-    <Card className="group overflow-hidden relative h-[400px]">
-      <CardHeader className="p-0">
-        <div className="relative w-full h-[280px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
+    <Card className="group overflow-hidden relative flex flex-col h-[350px]">
+      {/* Header */}
+      <CardHeader className="p-0 relative">
+        <Badge className="absolute top-4 left-4 z-10 bg-primary text-white px-3 py-1 text-xs font-semibold shadow-md">
+          {project.category}
+        </Badge>
+
+        <div className="relative w-full h-[200px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
           <Image
             className="absolute pt-4 rounded-lg"
             src={`/serviceSvg/${project.category}.svg`}
@@ -24,38 +37,52 @@ const ProjectCard = ({
             alt="project_image"
             priority
           />
-          <div>
-            {!isServiceCard && (
-              <div>
-                <Link
-                  href={`/servicos/${project.id}`}
-                  className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
-                >
-                  <CircleDollarSignIcon />
-                </Link>
-              </div>
-            )}
-          </div>
+          {!isServiceCard && (
+            <Link
+              href={`/servicos/${project.id}`}
+              className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
+            >
+              <CircleDollarSignIcon />
+            </Link>
+          )}
         </div>
       </CardHeader>
-      <div className="h-full px-8 py-3 xl:py-5 flex flex-col">
-        <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">
-          {project.category}
-        </Badge>
-        <h4 className="h4 text-center mb-1">{project.name}</h4>
+
+      {/* Content */}
+      <CardContent className="flex-1 h-full px-6 py-3 xl:py-5 flex flex-col">
+        <CardTitle className="text-base font-bold text-center mb-2">
+          {project.name}
+        </CardTitle>
         {isServiceCard ? (
+          <div className="flex flex-col gap-2 h-full justify-center items-center">
+            <div className="font-semibold">
+              {formatPriceToReais(project.price)}
+            </div>
+            <Link
+              href={`/servicos/${project.id}`}
+              className="flex gap-2 w-full h-full justify-end text-primary items-end text-sm"
+            >
+              <CalendarDaysIcon />
+            </Link>
+          </div>
+        ) : (
+          <CardDescription className="text-muted-foreground text-md line-clamp-2 flex-grow text-center">
+            {project.description}
+          </CardDescription>
+        )}
+      </CardContent>
+
+      {/* Footer */}
+      {!isServiceCard && (
+        <CardFooter className="flex justify-center py-3">
           <Link
             href={`/servicos/${project.id}`}
-            className="flex gap-5 justify-center text-primary underline text-center"
+            className="text-primary font-medium hover:underline"
           >
-            Solicitar Orçamento <CircleDollarSignIcon />
+            Ver detalhes
           </Link>
-        ) : (
-          <p className="text-muted-foreground text-md line-clamp-2 flex-grow">
-            {project.description}
-          </p>
-        )}
-      </div>
+        </CardFooter>
+      )}
     </Card>
   );
 };
